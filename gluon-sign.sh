a0e6aec5b99d478a060489c0276a316e5be26244
Felix Baumann <felix@dell.fritz.box>
2023-05-03 09:26:12 +0200

    improve gluon-sign.sh
    - autoload sign.sh
    - check whether ecdsautils is installed
    - add FIRMWAREPATH variable
    - update buildserverdomain

--- gluon-sign.sh
+++ gluon-sign.sh
@@ -3,7 +3,17 @@
 set -e
 FROM=2022.1.x
 BRANCH=experimental
+FIRMWAREPATH=/var/www/firmware/download
 
-scp ffac@community-build.freifunk-aachen.de:~/manifest/from-${FROM}/${BRANCH}.manifest from-${FROM}/${BRANCH}.manifest
-sign.sh  ~/.gluon-secret-key from-${FROM}/${BRANCH}.manifest
-scp from-${FROM}/${BRANCH}.manifest ffac@community-build.freifunk-aachen.de:~/manifest/from-${FROM}/${BRANCH}.manifest
+if ! command -v ecdsasign &> /dev/null
+then
+	echo "Please install ecdsautils"
+	exit
+fi
+
+wget -N https://raw.githubusercontent.com/freifunk-gluon/gluon/master/contrib/sign.sh > /dev/null 2>&1 || (echo "Check your network" && false)
+chmod 744 sign.sh
+mkdir -p from-${FROM}
+scp ffac@build.freifunk-aachen.de:${FIRMWAREPATH}/from-${FROM}/sysupgrade/${BRANCH}.manifest from-${FROM}/${BRANCH}.manifest
+./sign.sh  ~/.gluon-secret-key from-${FROM}/${BRANCH}.manifest
+scp from-${FROM}/${BRANCH}.manifest ffac@build.freifunk-aachen.de:$FIRMWAREPATH/from-${FROM}/sysupgrade/${BRANCH}.manifest
